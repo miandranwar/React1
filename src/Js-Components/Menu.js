d72b758ad9c3aa6e49907a1651ee7f18fea7c0b2
Muhammad <miandranwarg@gmail.com>
2020-05-08 23:13:30 +0100

    Added react-bootstrap

--- src/Js-Components/Menu.js
+++ src/Js-Components/Menu.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../Style/App.scss';
+import { Navbar, Nav } from 'react-bootstrap';
 
-class Menu extends Component {
-    constructor() {
-        super();
 
-        this.state = {
-            navigation: ['Home', 'News', 'Shopping', 'Returns', 'Contact us']
-        }
-    }
-    render() {
-        return (
-            <div className="menu">
-                {this.state.navigation.map((value, index) => {
-                    return  <a href="/">{value}</a>
-                })
-                }
-            </div>
-        );
-    }
+const Menu = () => {
+    return (
+        <div>
+            <Navbar  variant="dark" fixed ="top" className="menu">
+            <Navbar.Brand href="#home">Flix</Navbar.Brand>
+            <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            </Navbar>
+        </div>
+    );
 }
 
 export default Menu;
